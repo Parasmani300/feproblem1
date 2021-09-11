ce91fc9fc1f2d9e6b9c6cbe24973da25669c920b
Paras Mani <parasmani300@gmail.com>
2021-09-11 12:54:59 +0530

    reset button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState,useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import FinishScreen from './components/FinishScreen';
 import Footer from './components/Footer';
@@ -11,11 +13,23 @@ import Mission from './Pages/Mission';
 import StoryLine from './Pages/StoryLine';
 
 function App() {
+  const [key,setKey] = useState(0);
 
-
+  useEffect(() => {
+  }, [key])
   return (
-    <Router>
+    <Router key={key}>
       <Navigation />
+      <div className="container" >
+        <div className="row">
+          <div className="col-10"></div>
+          <div className="col-2">
+            <button className="btn btn-primary" onClick={() => setKey(key+1)}>
+                <Link to="/home" style={{color:'white',textDecoration:"none"}}>Reset</Link>
+              </button>
+          </div>
+        </div>
+      </div>
       <Switch>
           <Route path="/mission" >
               <Mission />
